8ed85a2e5d4cead89e16cced4a754b7592cbc262
saha-7op <saha7pritam@gmail.com>
2025-11-18 21:33:37 +0530

    mogoDb setup

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,7 +1,20 @@
 import express from 'express'
 import dotenv from "dotenv"
+import mongoose from 'mongoose'
 
 dotenv.config()
+
+const connectDb= async()=>{
+    try{
+        mongoose.connect(process.env.MONGO_URI as string,{
+            dbName: "Spotify",
+        })
+        console.log("MongoDB connected");
+        
+    }catch(error){
+        console.log(error)
+    }
+}
 
 const app=express()
 
@@ -13,4 +26,5 @@ const port = process.env.PORT || 5000
 
 app.listen(5000,()=>{
     console.log(`Server is running on port ${port}`)
+    connectDb()
 })
