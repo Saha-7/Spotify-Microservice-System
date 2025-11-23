8050e729ac114aa9acfd0c9948dad95bf4440fc5
saha-7op <saha7pritam@gmail.com>
2025-11-23 21:22:54 +0530

    myprofile route

--- user-service/src/route.ts
+++ user-service/src/route.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { loginUser, registerUser } from "./controller.js";
+import { loginUser, myProfile, registerUser } from "./controller.js";
+import { isAuth } from "./middleware.js";
 
 const userRouter = express.Router()
 
 userRouter.post("/user/register", registerUser)
 userRouter.post("/user/login", loginUser)
+userRouter.get("/user/my-profile", isAuth, myProfile)
 
 export default userRouter
